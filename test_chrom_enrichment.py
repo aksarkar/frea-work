23b56081ddbc5a8c63d99704c73c139873f3cacc
Abhishek Sarkar <aksarkar@mit.edu>
2011-08-17 16:01:38 -0400

    Split test for top SNPs and all SNPs
    Implement permutation test for all SNPs

--- test_chrom_enrichment.py
+++ test_chrom_enrichment.py
@@ -15,38 +15,59 @@ SNPs.
 
 """
 import csv
+import itertools as it
+import random
+import operator as op
 import sys
 
 import scipy.stats
 
-import annotate
 import bitvector
 
 fmt = '{},"{}",{:.6e},{:.1f}'
 
+def test_top(data, cell_type, states, top):
+    xs = [row['rsid'] for row in data if 
+          int(row[cell_type]) & bitvector.bitvector(states)]
+    xa = len([x for x in xs if x in top])
+    xb = len(xs) - xa
+    ya = len(top) - xa
+    yb = len(data) - len(xs) - ya
+    obs = xa / (xa + ya)
+    exp = len(xs) / len(data)
+    return fmt.format(cell_type, 
+                      ','.join(str(x) for x in states),
+                      scipy.stats.fisher_exact([[xa, xb], [ya, yb]])[1],
+                      obs / exp)
+def rot(iterable, offset):
+    return it.chain(it.islice(iterable, offset, None), 
+                    it.islice(iterable, 0, offset))
+
+def perm_helper(ps, annots, cell_type, states):
+    return sum(i for i, (p, a) in enumerate(zip(ps, annots)) 
+               if a & bitvector.bitvector(states))
+
+def perm(data, cell_type, states, nperms=None):
+    if nperms is None:
+        nperms = len(data)
+    ps, annots = zip(*sorted((float(row['p']), int(row[cell_type])) for row in data))
+    orig = perm_helper(ps, annots, cell_type, states)
+    count = sum(1 if perm_helper(ps, rot(annots, i), cell_type, states) <= orig 
+                else 0 for i in random.sample(range(len(data)), nperms))
+    return fmt.format(cell_type, 
+                      ','.join(str(x) for x in states),
+                      count / nperms, 1)
+
 def test(data, cell_type, states, top=[]):
     """Test for enrichment of p-values for given cell_type and states"""
-    mask = bitvector.bitvector(states)
-    xs = [row for row in data if int(row[cell_type]) & mask]
-    ys = [row for row in data if not int(row[cell_type]) & mask]
-    if not xs or not ys:
-        return fmt.format(cell_type, ','.join(str(x) for x in states), 1, 0)
-    elif top:
-        xa = len([x for x in xs if x['rsid'] in top])
-        xb = len(xs) - xa
-        ya = len([y for y in ys if y['rsid'] in top])
-        yb = len(ys) - ya
-        obs = xa / (xa + ya)
-        exp = len(xs) / len(data)
-        return fmt.format(cell_type, 
-                          ','.join(str(x) for x in states),
-                          scipy.stats.fisher_exact([[xa, xb], [ya, yb]])[1],
-                          obs / exp)
+    if top:
+        return test_top(data, cell_type, states, top)
     else:
-        raise NotImplementedError('RR test')
+        return perm(data, cell_type, states, 1000)
 
 if __name__ == '__main__':
-    data = list(csv.DictReader(sys.stdin))
+    reader = csv.DictReader(sys.stdin)
+    data = list(reader)
     top = []
     if len(sys.argv) > 1:
         with open(sys.argv[1]) as f:
