67b330c81a39109dfc918e16b9a107b78914d428
Abhishek Sarkar <aksarkar@mit.edu>
2011-09-07 18:15:44 -0400

    Fix input parsing for permutation test

--- perm_test/perm_test.c
+++ perm_test/perm_test.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ranksum(char *annots, int n, int offset) {
+int ranksum(int *annots, int n, int offset) {
   int result = 0;
   int rank = 0;
-  for (char *ap = annots + offset; ap < annots + n; ap++, rank++) {
-    if (*ap == '1') {
+  for (int *ap = annots + offset; ap < annots + n; ap++, rank++) {
+    if (*ap == 1) {
       result += rank;
     }
   }
-  for (char *ap = annots; ap < annots + offset; ap++, rank++) {
-    if (*ap == '1') {
+  for (int *ap = annots; ap < annots + offset; ap++, rank++) {
+    if (*ap == 1) {
       result += rank;
     }
   }
@@ -21,12 +21,11 @@ int ranksum(char *annots, int n, int offset) {
 int main(int argc, char **argv) {
   int n;
   scanf("%d", &n);
-  char *annots = malloc(n);
+  int *annots = calloc(n, sizeof(int));
   assert(annots != NULL);
-  for(char *ap = annots; ap < annots + n; ap++) {
-    scanf("%c", ap);
+  for(int *ap = annots; ap < annots + n; ap++) {
+    scanf("%d", ap);
   }
-  fclose(stdin);
   int orig = ranksum(annots, n, 0);
   int count = 0;
   for (int i = 1; i < n; i++) {
