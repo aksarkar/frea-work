bcd6c548ac4079aa4c31769ad629c0b66b22779e
Abhishek Sarkar <aksarkar@mit.edu>
2011-09-08 13:28:30 -0400

    Initial version of enhancer dips test helpers

---
+++ dips/dbgap_to_bed.py
@@ -0,0 +1,16 @@
+import csv
+import gzip
+import sys
+
+with gzip.open(sys.argv[1]) as f:
+    decode = (str(x, 'ascii') for x in f)
+    filter_comment = (x for x in decode if x.strip() and x[0] != '#')
+    reader = csv.DictReader(filter_comment, delimiter='\t')
+    filter_missing = (row for row in reader if row['P-value'] and 
+                      row['Chr Position'] and row['Chr ID'])
+    for row in filter_missing:
+        print('chr{}'.format(row['Chr ID']),
+              row['Chr Position'],
+              int(row['Chr Position']) + 1,
+              row['SNP ID'],
+              row['P-value'], sep='\t')
