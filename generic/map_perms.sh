39b6663b45ecccbf7c8d0eaafe1a82598da8bd62
Abhishek Sarkar <aksarkar@mit.edu>
2011-09-08 13:29:33 -0400

    Fix style (max line length)

--- generic/map_perms.sh
+++ generic/map_perms.sh
@@ -9,4 +9,5 @@
 
 perm_test="/seq/compbio-hp/GWAS/enrichment/scripts/perm_test/perm_test"
 sed -e "s/,/ /" $PT_WORK/joblist | \
-    parallel -j1 -C' ' "$SCRIPTS/filter $PT_WORK/annot {} | $perm_test" > $PT_WORK/map_perms.out
+    parallel -j1 -C' ' "$SCRIPTS/filter $PT_WORK/annot {} | $perm_test" > \
+    $PT_WORK/map_perms.out
