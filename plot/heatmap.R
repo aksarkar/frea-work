460bc6a51d4c33186f678241789b7406098d3f9e
Abhishek Sarkar <aksarkar@mit.edu>
2011-09-20 19:31:48 -0400

    Make internal-external state mapping explicit
    Move mapping into factor reordering

--- plot/heatmap.R
+++ plot/heatmap.R
@@ -6,14 +6,14 @@ library(Cairo)
 CairoFonts(regular="DejaVu Sans")
 args <- commandArgs(TRUE)
 D <- read.csv(args[1])
-D$state <- factor(D$state, labels=c('11','12','11,12','4','5','4,5','4,5,11,12'))
+D$external <- factor(D$state,
+                     levels=c('11','12','11,12','4','5','4,5','4,5,11,12'),
+                     labels=c('4', '5', 'strong', '6', '7', 'weak', 'all'))
 D$sig <- factor(D$p < .05 / 63, labels=c('', '\U2217'))
 hi <- ceiling(max(-log10(D$p)))
-p <- (qplot(data=D, x=state, y=cell_type, fill=-log10(p), label=sig, size=I(4), 
+p <- (qplot(data=D, x=external, y=cell_type, fill=-log10(p), label=sig, size=I(3), 
             geom=c('tile', 'text'), xlab = 'Enhancer state', ylab='Cell type') +
       scale_fill_gradient(low='white', high='blue', limits=c(0, hi)) +
-      scale_x_discrete(breaks=c('11','12','11,12','4','5','4,5','4,5,11,12'),
-                       labels=(c('4','5','strong','6','7','weak','all'))) +
       coord_equal() +
       theme_bw())
 Cairo(file='out.pdf', type='pdf', dpi=96)
