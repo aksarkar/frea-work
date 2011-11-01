cf8d4d305d7180dd520c2744b2049e6d98ee185e
Abhishek Sarkar <aksarkar@mit.edu>
2011-11-01 15:16:34 -0400

    Only use binary annotations

--- enr/chrom/bin_celltypes.py
+++ enr/chrom/bin_celltypes.py
@@ -11,8 +11,7 @@ types = [str, float] + [int for _ in celltypes]
 data = [[int(x) for x in xs[2:]] for xs in
         sorted(list(r), key=lambda row: float(row[1]))]
 
-enhancers = 1
-expected = [len([x for x in xs if x & enhancers]) / len(xs) for xs in
+expected = [len([x for x in xs if x & 1]) / len(xs) for xs in
             zip(*data)]
 
 binsize = int(sys.argv[1])
@@ -21,7 +20,7 @@ w = csv.writer(sys.stdout)
 w.writerow(['total', 'feature', 'count', 'expected'])
 for i in range(0, len(data), binsize):
     count = [c + x for c, x in 
-             zip(count, [len([x for x in xs if x & enhancers]) for
+             zip(count, [len([x for x in xs if x & 1]) for
                          xs in zip(*data[i:i + binsize])])]
     for c, type_, e in zip(count, celltypes, expected):
         w.writerow([i + binsize, type_, c, int((i + binsize) * e)])
