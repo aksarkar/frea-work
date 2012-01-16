e891fdda7b894b76b7e765daa3b78066fff74dc9
Abhishek Sarkar <aksarkar@mit.edu>
2012-01-16 01:23:25 -0500

    Do not assume data is compressed
    Use correct fields of data

--- util/conv.sh
+++ util/conv.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#BSUB -J convert[1-25]
+#BSUB -J convert[1-13]
 #BSUB -o convert.%J
 #BSUB -q compbio-week
-in=$(sed -ne "$LSB_JOBINDEX p" $HOME/args)
-out=$HOME/tmp/$(echo $in | sed -nre "s#.*/([^.]*)\..*#\1#p").bed.gz
-zcat $in | \
-    awk '/^[^#]/' | \
-    awk -vOFS='\t' 'NR > 1 && $12 != "NA" {print "chr"$3, $4, $4 + 1, $1, $12}' | \
+in=$(sed -ne "$LSB_JOBINDEX p" $HOME/temp)
+out=$HOME/tmp/geneva/$(echo $in | sed -nre "s#.*/([^.]*)\..*#\1#p").bed.gz
+echo $out
+awk '/^[^#]/' <$in | \
+    awk -vOFS='\t' 'NR > 1 && $10 != "NA" {print "chr"$3, $4, $4 + 1, $1, $10}' | \
     gzip >$out
