77a92c5392b308c9033573bf82830e5394762175
Abhishek Sarkar <aksarkar@mit.edu>
2012-01-16 01:14:34 -0500

    Update paths to ChromHMM data

--- enr/fisher/count.sh
+++ enr/fisher/count.sh
@@ -7,24 +7,20 @@ gwas=$1
 top=$2
 not=$(mktemp -p /broad/shptmp/aksarkar)
 intersectBed -v -a $gwas -b $2 >$not
-# python $HOME/code/enr/fisher/filter.py $2 $1 >$not
 
 wc -l <$top
 wc -l <$not
 
-work=$(mktemp -p /broad/shptmp/aksarkar)
 for c in ${ctypes[@]}
 do
     for e in ${enh[@]}
     do
         echo "$c $e"
-        bzcat $HOME/hp/chromhmm/$c-states.bed.bz2 | grep $e | \
-            intersectBed -a $top -b stdin -c >$work
-        grep '1$' $work | wc -l
+        zcat $HOME/hp/chromhmm/$c.bed.gz | grep $e | \
+            intersectBed -a $top -b stdin | wc -l
 
-        bzcat $HOME/hp/chromhmm/$c-states.bed.bz2 | grep $e | \
-            intersectBed -a $not -b stdin -c >$work
-        grep '1$' $work | wc -l
+        zcat $HOME/hp/chromhmm/$c.bed.gz | grep $e | \
+            intersectBed -a $not -b stdin | wc -l
     done
 done
-rm $work $not
+rm $not
