21290c08a11dd1b0a59c70fd2513aa246280428b
Abhishek Sarkar <aksarkar@mit.edu>
2012-01-16 01:19:47 -0500

    Color points by annotation
    Write to Postscript instead of PNG

--- enr/plot/mhtplot.R
+++ enr/plot/mhtplot.R
@@ -4,6 +4,13 @@
 library(Cairo)
 library(ggplot2)
 
+scale_state_aggregate <-
+  scale_color_manual(name='State (aggregate)',
+                     values=c('promoter' = '#ff0000', 'enhancer' = '#faca00',
+                       'insulator' = '#09befe', 'transcribed' = '#00b050',
+                       'repressed' = '#7f7f7f', 'other' = 'black',
+                       'coding' = '#005c1f', 'diffexpr' = '#0060a0'))
+
 CairoFonts(regular='Dejavu Sans')
 args <- commandArgs(TRUE)
 df <- read.csv(args[1])
@@ -13,14 +20,14 @@ lens <- c(0, 245203898, 243315028, 199411731, 191610523, 180967295, 170740541,
           63790860, 63644868, 46976537, 49476972, 152634166, 50961097)
 xs <- cumsum(lens)
 df$x = xs[df$chr] + df$pos
-p <- (qplot(data=df, x=pos / 1e6, xlab='Position (Mb)', y=-log10(p), geom='point',
-            color=factor(enh, levels=c('0', '1'), labels=c('False', 'True'))) +
-      scale_x_continuous(limits=c(0, 50)) +
-      scale_color_manual(values=c('True'='#faca00', 'False'='black'), legend=FALSE) +
+p <- (qplot(data=df, x=x, xlab='Genomic position', y=-log10(p), geom='point',
+            color=state, size=I(1)) +
+      scale_x_continuous(breaks=xs, labels=seq(1, 25)) +
+      scale_state_aggregate +
       geom_hline(color='red', yintercept=6) +
       scale_y_continuous(limits=c(0, ceiling(max(-log10(df$p))))) +
       opts(legend.position='none') +
       theme_bw())
-CairoPNG(file='out.png', height=512, width=1024)
+postscript()
 print(p)
 dev.off()
