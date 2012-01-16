55ad3b6dca91f6e7a8c864dca296a8a1f066766e
Abhishek Sarkar <aksarkar@mit.edu>
2012-01-16 01:16:17 -0500

    Reverse cell type labels (alphabetical as you go down)
    Fix midpoint and limits of color scale

--- enr/plot/fisher_heatmap.R
+++ enr/plot/fisher_heatmap.R
@@ -6,19 +6,16 @@ library(Cairo)
 args <- commandArgs(TRUE)
 D <- read.csv(args[1])
 D$cell_type <- factor(D$cell_type,
-                      levels=c('gm12878', 'h1', 'hepg2', 'hmec', 'hsmm',
-                        'huvec', 'k562', 'nhek', 'nhlf'),
-                      labels=c('GM12878', 'H1', 'HepG2', 'HMEC', 'HSMM',
-                        'Huvec', 'K562', 'NHEK', 'NHLF'))
-if (! 'fold' %in% colnames(D)) {
-  D$fold <- factor(D$p < .05 / 63, labels=c('', '\U2217'));
-}
+                      levels=c('nhlf', 'nhek', 'k562', 'huvec', 'hsmm', 'hmec',
+                        'hepg2', 'h1', 'gm12878'),
+                      labels=c('NHLF', 'NHEK', 'K562', 'Huvec', 'HSMM', 'HMEC',
+                        'HepG2', 'H1', 'GM12878'))
 hi <- 12
 p <- (qplot(data=D, x=state, y=cell_type, fill=fold,
             label=sprintf("%.1g", p), size=I(3), 
             geom=c('tile', 'text'), xlab = 'Enhancer state', ylab='Cell type') +
       scale_fill_gradient2(high='#ff0000', mid='white', low='#b0b0ff',
-                           midpoint=0) +
+                           midpoint=1, limits=c(0, hi)) +
       scale_x_discrete(limits=c('strong', 'weak', 'all')) +
       coord_equal() +
       theme_bw())
