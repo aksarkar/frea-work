44ae77fd43bdca93efff8f682eda4fb31023b67f
Abhishek Sarkar <aksarkar@mit.edu>
2012-03-02 16:25:58 -0500

    Change input format to match new pipeline
    The pipeline keeps everything in BED format until this point.

--- ld/torel.py
+++ ld/torel.py
@@ -2,14 +2,15 @@
 
 Author: Abhishek Sarkar
 
-Expects BED4 (representative, 0-based start, 0-based end, annotation) on stdin.
-Writes (representative, relative start, relative end, annotation) on stdout.
+Expects BED5 (chr, start, end, annotation, representative) on stdin. Writes CSV
+(group, relative start, relative end, annotation) on stdout.
 """
 import csv
 import itertools as it
 import operator as op
 import sys
 
-for i, (_, xss) in enumerate(it.groupby(csv.reader(sys.stdin), key=op.itemgetter(0))):
+for i, (_, xss) in enumerate(it.groupby(csv.reader(sys.stdin, delimiter='\t'),
+                                        key=op.itemgetter(4))):
     for xs in xss:
-        print(i, int(xs[1]) - int(xs[0]), int(xs[2]) - int(xs[0]), xs[3], sep=',')
+        print(i, int(xs[1]) - int(xs[4]), int(xs[2]) - int(xs[4]), xs[3], sep=',')
