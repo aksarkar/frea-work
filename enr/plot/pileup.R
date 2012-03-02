f9172102d37ff268e782701c485dfcd79d43205e
Abhishek Sarkar <aksarkar@mit.edu>
2012-03-02 16:24:10 -0500

    Make plot easier to interpret
    Distinguish exons from protein-coding transcribed.
    
    Fix misleading y-axis label and scale: use log-scale, not log-transformed
    values.
    
    Limit regions to +/- 200kb

--- enr/plot/pileup.R
+++ enr/plot/pileup.R
@@ -9,7 +9,7 @@ scale_state_aggregate <-
                      values=c('promoter' = '#ff0000', 'enhancer' = '#faca00',
                        'insulator' = '#09befe', 'transcribed' = '#00b050',
                        'repressed' = '#7f7f7f', 'other' = 'black',
-                       'coding' = '#005c1f', 'protein_coding' = '#0060a0',
+                       'coding' = '#005c1f', 'exon' = '#0060a0',
                        'known_lincRNA' = '#a000a0', 'putative_lincRNA' = '#ff00ff'))
 
 args <- commandArgs(TRUE)
@@ -17,7 +17,9 @@ d <- read.csv(args[1], header=0)
 CairoPDF(file='out.pdf', width=11, height=8.5)
 print(qplot(data=d, x=V1, y=V3, geom='freqpoly', stat='identity',
             color=factor(V2), xlab='Relative position',
-            ylab='Log-transformed count') +
+            ylab='Count') +
+      scale_x_continuous(limits=c(-200e3, 200e3)) +
+      scale_y_log10() +
       scale_state_aggregate +
       theme_bw())
 dev.off()
