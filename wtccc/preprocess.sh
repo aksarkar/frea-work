fe48ed9c4f9dd21640bf7dafa48778b47c624c95
Abhishek Sarkar <aksarkar@mit.edu>
2012-06-21 11:58:20 -0400

    Make sure to use python3 to load chromosomes

--- wtccc/preprocess.sh
+++ wtccc/preprocess.sh
@@ -4,4 +4,4 @@
 #BSUB -o log
 #BSUB -q compbio-week
 awk -v chr=chr$LSB_JOBINDEX '$1 == chr {print $4}' markers.bed | \
-    python $HOME/code/wtccc/load.py $LSB_JOBINDEX
+    python3 $HOME/code/wtccc/load.py $LSB_JOBINDEX
