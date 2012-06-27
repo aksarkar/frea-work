ba7d87c2bd734f9e9084e7abd2a24954d502f343
Abhishek Sarkar <aksarkar@mit.edu>
2012-06-27 00:45:42 -0400

    Output all intermediate solutions
    Output model coefficients, test set, probability of positive class, ROC curves
    per iteration and threshold average ROC curve

--- wtccc/classifier.py
+++ wtccc/classifier.py
@@ -1,7 +1,8 @@
 """Logistic regression for WTCCC T1D
 
-Computes ROC curves for L1-penalized logistic regression classifiers with
-stratified 20-fold cross validation.
+Performs L1-penalized logistic regression with 20-fold stratified
+cross-validation. Outputs model coefficients, probabilities of positive class,
+individual ROC curves, and threshold averaged ROC curve.
 
 Usage: python2 classifier.py GENOTYPES
 
@@ -11,6 +12,7 @@ Author: Abhishek Sarkar <aksarkar@mit.edu>
 from __future__ import print_function
 from __future__ import division
 
+import functools
 import itertools
 import sys
 
@@ -19,19 +21,98 @@ import sklearn.cross_validation as cv
 import sklearn.linear_model as lm
 import sklearn.metrics as m
 
-A = numpy.array
-with open(sys.argv[1]) as f:
-    rows = [[int(x) for x in xs.split()[1:]] for xs in f]
-    X = A(zip(*rows))
-y = A([1 for _ in xrange(2000)] + [0 for _ in xrange(3004)])
-model = lm.LogisticRegression(penalty='l1')
-for i, (train, test) in enumerate(cv.StratifiedKFold(y, 20)):
+def roc(ps, y, weight=.5):
+    """Yield points (fpr, tpr, beta) on the ROC curve"""
+    yield 0, 0, 0
+    for t in sorted(set(ps)):
+        y_ = [1 if p > t else 0 for p in ps]
+        tp, fn, fp, tn = m.confusion_matrix(y, y_).flatten()
+        tpr = tp / (tp + fn)
+        fpr = fp / (fp + tn)
+        yield fpr, tpr, m.fbeta_score(y, y_, weight)
+
+def mean(xs):
+    """Return the mean of list xs"""
+    return sum(xs) / len(xs)
+
+def centroid(xs, ys, zs):
+    """Return the centroid of points (x, y, z)"""
+    return mean(xs), mean(ys), mean(zs)
+
+def at(rocs, thresh):
+    """Return the greatest point with score less than thresh for each ROC
+    curve"""
+    def helper(r, t):
+        return max((s, i) for i, (_, _, s) in enumerate(r) if s <= t)[1]
+    indexes = (helper(r, thresh) for r in rocs)
+    return [r[i] for r, i in zip(rocs, indexes)]
+
+def avg_roc(rocs, nsamples=200):
+    """Yield points (fpr, tpr, beta) on the treshold averaged ROC curve"""
+    scores = sorted(s for x, y, s in itertools.chain(*rocs))
+    thresholds = (scores[i] for i in range(0, len(scores),
+                                           len(scores) // nsamples))
+    yield 0, 0, 0
+    for t in thresholds:
+        yield centroid(*list(zip(*at(rocs, t))))
+    yield 1, 1, 0
+
+def area(p, q, r):
+    """Return the signed area of the triangle (p, q, r)"""
+    px, py = p[:2]
+    qx, qy = q[:2]
+    rx, ry = r[:2]
+    return 5 * (-qx * py + rx * py + px * qy - rx * qy - px * ry + qx * ry)
+
+def hull(ps):
+    """Return the convex hull of list of points ps.
+
+    Assume points are sorted by x, monotonically increasing in y.
+
+    """
+    h, ps = ps[:2], ps[1:]
+    while ps:
+        if len(h) > 1:
+            if area(h[-2], h[-1], ps[0]) < 0:
+                h.append(ps.pop(0))
+            else:
+                h.pop()
+        else:
+            h.append(ps.pop(0))
+    return h
+
+def run(model, X, y, train, test):
+    """Run one iteration of cross-validation.
+
+    Return model coefficients, predictions, and ROC curve
+
+    """
     model.fit(X[train], y[train])
     ps = model.predict_proba(X[test])[:,1]
-    with open('roc.{:02d}'.format(i), 'w') as f:
-        for t in sorted(set(ps)):
-            y_ = [1 if p > t else 0 for p in ps]
-            tp, fn, fp, tn = m.confusion_matrix(y[test], y_).flatten()
-            tpr = tp / (tp + fn)
-            fpr = fp / (fp + tn)
-            print(fpr, tpr, m.fbeta_score(y[test], y_, .5), file=f)
+    return model.coef_, ps
+
+if __name__ == '__main__':
+    A = numpy.array
+    with open(sys.argv[1]) as f:
+        X = A(zip(*[[int(x) for x in xs.split()[1:]] for xs in f]))
+    y = A([1 for _ in xrange(2000)] + [0 for _ in xrange(3004)])
+    model = lm.LogisticRegression(penalty='l1')
+    run_ = functools.partial(run, model, X, y)
+    cv_ = cv.StratifiedKFold(y, 20)
+    css, pss = zip(*[run_(train, test) for train, test in cv_])
+    with open('models.csv', 'w') as f:
+        for i, cs in enumerate(css):
+            for c in cs.flatten():
+                print(i, c, sep=',', file=f)
+    with open('predictions.csv', 'w') as f:
+        for i, (ps, (_, test)) in enumerate(zip(pss, cv_)):
+            for p, t in zip(ps, test):
+                print(i, t, p, y[t], sep=',', file=f)
+    rocs = [sorted(roc(ps, y[test])) for ps, (_, test) in zip(pss, cv_)]
+    with open('rocs.csv', 'w') as f:
+        for i, roc in enumerate(rocs):
+            for fpr, tpr, score in roc:
+                print(i, fpr, tpr, score, sep=',', file=f)
+    with open('avg.csv', 'w') as f:
+        for tpr, fpr, _ in sorted(hull(list(avg_roc(rocs)))):
+            print(tpr, fpr, sep=',', file=f)
--- wtccc/classifier.sh
+++ wtccc/classifier.sh
@@ -4,4 +4,3 @@
 #BSUB -q compbio-week
 set -e
 python $HOME/code/wtccc/classifier.py merged
-python3 $HOME/code/wtccc/avg_roc.py $NSAMPLES $LABEL roc.* >avg.csv
