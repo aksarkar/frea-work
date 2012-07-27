196a38534fab65d7abf0bd7a4cc0ff339a1743e3
Abhishek Sarkar <aksarkar@mit.edu>
2012-07-26 21:58:28 -0400

    Use KS 2-sample test instead of exact ranksum test for DGF enrichment
    Empirical p-values can be no small than 1e-6, which raises a problem with
    multiple testing correction (nothing survives). Use KS instead as a first pass
    over the data to find interesting phenotype/cell type enrichments.

--- enr/dgf/dbgap.sh
+++ enr/dgf/dbgap.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 #BSUB -J dbgap-dgf[1-4215]
-#BSUB -R rusage[mem=6,argon_io=3]
+#BSUB -R rusage[mem=1,argon_io=3]
 #BSUB -o dbgap-dgf.log
 #BSUB -q compbio-week
 set -e
-tmp=/broad/hptmp/aksarkar
-markers=$(find /broad/compbio/aksarkar/dbgap/analyses -type f | \
+dbgap=/broad/compbio/aksarkar/dbgap
+markers=$(find $dbgap/analyses -type f | \
     sort | \
     sed -n "$LSB_JOBINDEX p")
-expanded=$tmp/$(printf "%04d" $LSB_JOBINDEX).bed.gz
-zcat $markers | \
-    $HOME/code/ld/hapmap/expand.sh .8 | \
-    gzip >$expanded
+expanded=$dbgap/expanded/hapmap/$(printf "%04d" $LSB_JOBINDEX).bed.gz
 annotations=/broad/compbio/aksarkar/annotations
-t=$(mktemp -p $tmp)
+t=$(mktemp -p /broad/hptmp/aksarkar)
 for i in $(find $annotations/dgf/ -type f | \
     sort | \
     paste -d, - $annotations/celltypes/dgf-celltypes.txt)
 do
     annot=$(echo $i | cut -d, -f1)
     celltype=$(echo $i | cut -d, -f2)
-    bedtools intersect -a $expanded -b $annot -c | \
-        python $HOME/code/ld/group.py union | \
-        sort -g -k5 | cut -f7 >$t
     echo -n ">>> $markers $celltype "
-    wc -l <$t | cat - $t | $HOME/code/enr/exact/ranksum
+    bedtools intersect -a $expanded -b $annot -c | \
+        python3 $HOME/code/ld/group.py union | \
+        sort -g -k5 | \
+        cut -f5,7 | \
+        python $HOME/code/test/ks.py
 done
 rm $t
