72aef0f0d5bbd759675e02c79e040adcf9a7c115
Abhishek Sarkar <aksarkar@mit.edu>
2012-07-26 21:36:58 -0400

    Fix dbGaP processing for Luke Ward's pipeline
    Fix style of pipeline.
    
    Use updated 1KG expansion.

--- enr/go/dbgap_to_luke.sh
+++ enr/go/dbgap_to_luke.sh
@@ -7,7 +7,9 @@ set -e
 in=$(find /broad/compbio/aksarkar/dbgap/analyses -type f | sort | \
     sed -n "$LSB_JOBINDEX p")
 ld=/broad/compbio/aksarkar/annotations/1kg-ld
-zcat $in | python $HOME/code/util/dbgap_to_bed.py | \
+zcat $in | \
+    python $HOME/code/util/dbgap_to_bed.py | \
     bedtools intersect -a stdin -b /broad/hptmp/aksarkar/haploreg.bed.gz -wb | \
     awk '{print $9, $5}' | \
-    python $HOME/code/ld/expand_1kg.py $ld/CEU.txt $ld/ceu-index.txt >$LSB_JOBINDEX.out
+    python $HOME/code/ld/1kg/expand.py $ld/ceu-index.txt $ld/CEU.txt 0.8 | \
+    gzip >$LSB_JOBINDEX.out.gz
