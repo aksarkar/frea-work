75363b56e04a1078258c40b5fa5f00e7a19e513d
Abhishek Sarkar <aksarkar@mit.edu>
2012-07-31 21:15:53 -0400

    Update input and output paths

--- enr/go/dbgap_to_luke.sh
+++ enr/go/dbgap_to_luke.sh
@@ -6,10 +6,10 @@
 set -e
 in=$(find /broad/compbio/aksarkar/dbgap/analyses -type f | sort | \
     sed -n "$LSB_JOBINDEX p")
-ld=/broad/compbio/aksarkar/annotations/1kg-ld
+ld=/broad/compbio/aksarkar/ld/1kg
 zcat $in | \
     python $HOME/code/util/dbgap_to_bed.py | \
-    bedtools intersect -a stdin -b /broad/hptmp/aksarkar/haploreg.bed.gz -wb | \
+    bedtools intersect -a stdin -b $ld/haploreg.bed -wb | \
     awk '{print $9, $5}' | \
     python $HOME/code/ld/1kg/expand.py $ld/ceu-index.txt $ld/CEU.txt 0.8 | \
-    gzip >$LSB_JOBINDEX.out.gz
+    gzip >$(printf "%04d" $LSB_JOBINDEX).out.gz
