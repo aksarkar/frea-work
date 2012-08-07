e12a42b68157fc457685b80734784980d0c45100
Abhishek Sarkar <aksarkar@mit.edu>
2012-08-07 18:26:00 -0400

    Remove generic test framework
    The framework ended up being too inflexible for new kinds of annotations. In
    particular, dealing with LD was problematic.
    
    It also did not have good granularity of tasks and was not flexible enough to
    adjust.
    
    Finally, the general method for intersecting, etc. was further developed
    separately for T1D and diverged greatly. We found generalizing from that code
    was easier and had nicer properties (granularity, simplicity).

--- enr/chrom/annot_coding.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ctypes=(gm12878 h1 hepg2 hmec hsmm huvec k562 nhek nhlf)
-
-for c in ${ctypes[@]}
-do
-    zcat $HOME/hp/expr/$c.cds.bed.gz | \
-        intersectBed -a $1 -b stdin -c | cut -f6 >$c.annot
-done
-cut -f4,5 $1 | paste - *.annot | sed -e "s/\t/,/g"
