76bd1c049f475d337e0d9e84413ebcbcd06b212a
Abhishek Sarkar <aksarkar@mit.edu>
2012-08-23 00:19:43 -0400

    Fix braindead implementation of exact ranksum test
    Expect markers in genomic order on stdin instead of rank order. Before, we were
    rotating ranks which is totally wrong (doesn't preserve genome structure, which
    was the point of rotation in the first place).

--- test/exact/ranksum.c
+++ test/exact/ranksum.c
@@ -1,6 +1,21 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+static int cmp(const void *l, const void *r) {
+  double x = *(double*)l;
+  double y = *(double*)r; 
+  if (x < y) {
+    return -1;
+  }
+  else if (x > y) {
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
 
 static inline void update(int *annot, int *rank, long *result) {
   if (*annot == 1) {
@@ -8,14 +23,14 @@ static inline void update(int *annot, int *rank, long *result) {
   }
 }
 
-long ranksum(int *annots, int n, int offset) {
+long ranksum(int *rs, int *xs, int n, int offset) {
   long result = 0;
-  int rank = 0;
-  for (int *ap = annots + offset; ap < annots + n; ap++, rank++) {
-    update(ap, &rank, &result);
+  int *r = rs;
+  for (int *ap = xs + offset; ap < xs + n; ap++, r++) {
+    update(ap, r, &result);
   }
-  for (int *ap = annots; ap < annots + offset; ap++, rank++) {
-    update(ap, &rank, &result);
+  for (int *ap = xs; ap < xs + offset; ap++, r++) {
+    update(ap, r, &result);
   }
   return result;
 }
@@ -23,15 +38,40 @@ long ranksum(int *annots, int n, int offset) {
 int main(int argc, char **argv) {
   int n;
   scanf("%d", &n);
-  int *annots = calloc(n, sizeof(int));
-  assert(annots != NULL);
-  for(int *ap = annots; ap < annots + n; ap++) {
-    scanf("%d", ap);
+  double *ps = calloc(n, sizeof(double));
+  assert(ps != NULL);
+  int *xs = calloc(n, sizeof(int));
+  assert(xs != NULL);
+  {
+    double *p;
+    int *x;
+    for(p = ps, x = xs; x < xs + n; p++, x++) {
+      scanf("%lf %d", p, x);
+    }
   }
-  long orig = ranksum(annots, n, 0);
+
+  double *qs = calloc(n, sizeof(double));
+  assert(qs != NULL);
+  memcpy(qs, ps, n * sizeof(double));
+  qsort(qs, n, sizeof(double), cmp);
+  int *rs = calloc(n, sizeof(int));
+  assert(rs != NULL);
+  {
+    double *p;
+    int *r;
+    for (p = ps, r = rs; p < ps + n; p++, r++) {
+      void *t = bsearch(p, qs, n, sizeof(double), cmp);
+      assert(t != NULL);
+      *r = (double*)t - qs;
+    }
+  }
+  free(ps);
+  free(qs);
+
+  long orig = ranksum(rs, xs, n, 0);
   int count = 0;
   for (int i = 1; i < n; i++) {
-    if (ranksum(annots, n, i) <= orig) {
+    if (ranksum(rs, xs, n, i) <= orig) {
       count++;
     }
   }
