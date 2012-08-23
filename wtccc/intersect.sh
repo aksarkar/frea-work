3ecef24a55b96d5b5eb157acfb4c65908e12e4c0
Abhishek Sarkar <aksarkar@mit.edu>
2012-08-23 00:23:56 -0400

    Subtract out WTCCC1 exclude list when comparing expanded and imputed p-values

--- wtccc/intersect.sh
+++ wtccc/intersect.sh
@@ -4,6 +4,7 @@
 #BSUB -o intersect-wtccc1.log
 #BSUB -q compbio-week
 set -e
+echo "study imputed expanded" >out.txt
 for i in {1..7}
 do
     imputed=$(find /broad/compbio/aksarkar/wtccc1/imputed -type f | \
@@ -13,6 +14,7 @@ do
         sort | \
         sed -n "$i p")
     study=$(basename $imputed | sed 's/.bed.gz//')
-    bedtools intersect -a $imputed -b $expanded -wb | \
-        awk -vs=$study 'BEGIN {print "study", "imputed", "expanded"} {print s, $5, $10}' >>out.txt
+    bedtools intersect -a $imputed -b /broad/compbio/aksarkar/wtccc1/exclude.bed.gz -v | \
+        bedtools intersect -a stdin -b $expanded -wb | \
+        awk -vs=$study '{print s, $5, $10}' >>out.txt
 done
