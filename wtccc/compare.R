09379987b5a39d48d8364e902446b6a3fc5c30e2
Abhishek Sarkar <aksarkar@mit.edu>
2012-08-23 00:23:19 -0400

    Plot distribution of differences between expanded and imputed p-values

--- wtccc/compare.R
+++ wtccc/compare.R
@@ -2,13 +2,22 @@
 # Author: Abhishek Sarkar <aksarkar@mit.edu>
 library(Cairo)
 library(grid)
+library(ggplot2)
 library(hexbin)
 
 args <- commandArgs(TRUE)
 d <- read.table(args[1], header=TRUE, sep=' ')
-p <- hexbin(d$imputed, d$expanded, xlab='Imputed p-value',
-            ylab='Representative p-value', xbins=100)
-CairoPNG(file='heatmap.png', height=1024, width=1024)
-grid.newpage()
-plot(p)
+d$diff = d$expanded - d$imputed
+p1 <- (qplot(data=d, x=diff, color=factor(study),
+             xlab='Representative p-value minus imputed p-value',
+             geom='freqpoly', binwidth=.05) +
+       scale_color_brewer(palette='Set3', name='Study') +
+       theme_bw())
+p2 <- hexbin(d$imputed, d$expanded, xlab='Imputed p-value',
+             ylab='Representative p-value', xbins=100)
+pdf(file='compare-dist.pdf', height=6, width=6)
+print(p1)
+dev.off()
+pdf(file='compare-hexbin.pdf', height=6, width=6)
+plot(p2)
 dev.off()
