94c485a3bed3aac896f2c3910240704b8f7df993
Abhishek Sarkar <aksarkar@mit.edu>
2012-08-27 16:25:51 -0400

    Refactor plotting code
    Add more descriptive scale labels. Paramterize the color scale for the plot.

--- enr/plot/rrplot.R
+++ enr/plot/rrplot.R
@@ -3,11 +3,6 @@
 library(ggplot2)
 library(Cairo)
 
-args <- commandArgs(TRUE)
-D <- read.csv(args[1])
-D$denom = rep(tail(D$expected, length(table(D$feature))), length.out=length(D$count))
-D$dev <- (D$count - D$expected) / D$denom
-
 scale_cell_type <-
   scale_color_manual(name='Cell type',
                      values=c('H1' = '#3e8f54', 'Huvec' = '#e2560f',
@@ -23,12 +18,23 @@ scale_state_aggregate <-
                        'repressed' = '#7f7f7f', 'other' = 'black',
                        'Poly-A+ RNA-Seq' = '#005c1f', 'diffexpr' = '#0060a0'))
 
-p <- (qplot(data=subset(D, total < 150000), x=total, y=dev, geom='line',
-            size=I(.5), color=feature, xlab='Rank', ylab='Deviation') + 
-      geom_hline(yintercept=0) + 
-      scale_state_aggregate +
-      scale_x_continuous(breaks=c(0, 50000, 100000, 150000)) +
-      theme_bw())
-Cairo(type='pdf', file='out.pdf', dpi=96)
+rrplot <- function(D, colors) {
+  return (qplot(data=D, x=total, y=dev, geom='line', size=I(.25), color=feature,
+                xlab='SNPs ranked by p-value',
+                ylab='Normalized deviation from expected count') + 
+          colors +
+          scale_x_continuous(limits=c(0, 150000),
+                             breaks=c(0, 50000, 100000, 150000)) +
+          scale_y_continuous(limits=c(0, max(D$dev))) +
+          theme_bw())
+}
+
+args <- commandArgs(TRUE)
+D <- read.csv(args[1])
+D$denom = rep(tail(D$expected, length(table(D$feature))), length.out=length(D$count))
+D$dev <- (D$count - D$expected) / D$denom
+p <- rrplot(subset(D, total < 150000),
+            scale_color_brewer(name='disease x feature x celltype', palette='Set1'))
+Cairo(type='pdf', file='out.pdf', dpi=96, width=8, height=5, units='in')
 print(p)
 dev.off()
