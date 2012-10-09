429a58132fec47f7ceb5440051ac9553063530c9
Abhishek Sarkar <aksarkar@mit.edu>
2012-10-08 22:51:04 -0400

    Fix faceted RR plots
    Compute deviations per phenotype/feature/cell type combination properly (by
    splitting the data frame).

--- enr/plot/rrplot.R
+++ enr/plot/rrplot.R
@@ -5,7 +5,7 @@ library(Cairo)
 
 scale_cell_type <-
   scale_color_manual(name='Cell type',
-                     values=c('H1' = '#3e8f54', 'Huvec' = '#e2560f',
+                     values=c('H1' = '#3e8f54', 'HUVEC' = '#e2560f',
                        'GM12878' = '#9e320e', 'HepG2' = '#1182bb',
                        'HMEC' = '#3fe7d2', 'K562' = '#3c11bc',
                        'NHEK' = '#bb1d94', 'HSMM' = '#e0c31b',
@@ -18,23 +18,29 @@ scale_state_aggregate <-
                        'repressed' = '#7f7f7f', 'other' = 'black',
                        'Poly-A+ RNA-Seq' = '#005c1f', 'diffexpr' = '#0060a0'))
 
-rrplot <- function(D, colors) {
-  return (qplot(data=D, x=total, y=dev, geom='line', size=I(.25), color=feature,
-                xlab='SNPs ranked by p-value',
-                ylab='Normalized deviation from expected count') + 
-          colors +
-          scale_x_continuous(limits=c(0, 150000),
-                             breaks=c(0, 50000, 100000, 150000)) +
-          scale_y_continuous(limits=c(0, max(D$dev))) +
-          theme_bw())
+dev <- function(X, binsize=1000) {
+  return(data.frame(X$total, (X$count - X$expected) / max(X$count)))
+}
+
+rrplot <- function(X) {
+  return(qplot(data=X, x=total, y=dev, geom='line', size=I(.25),
+               color=celltype, xlab='SNPs ranked by p-value',
+               ylab='Normalized deviation from expected count') +
+         scale_x_continuous(limits=c(0, 150000),
+                            breaks=c(0, 50000, 100000, 150000)) +
+         geom_hline(yintercept=0, color='black') +
+         theme_bw() +
+         opts(strip.background=theme_rect(fill=NA, colour=NA)))
 }
 
 args <- commandArgs(TRUE)
 D <- read.csv(args[1])
-D$denom = rep(tail(D$expected, length(table(D$feature))), length.out=length(D$count))
-D$dev <- (D$count - D$expected) / D$denom
-p <- rrplot(subset(D, total < 150000),
-            scale_color_brewer(name='disease x feature x celltype', palette='Set1'))
-Cairo(type='pdf', file='out.pdf', dpi=96, width=8, height=5, units='in')
+E <- ddply(D, .(phenotype, feature, celltype), dev)
+rm(D)
+colnames(E) <- c('phenotype', 'feature', 'celltype', 'total', 'dev')
+p <- (rrplot(subset(E, total < 150000)) +
+      facet_grid(phenotype ~ feature, scale='free'))
+Cairo(type='pdf', file='out.pdf', dpi=96, width=28, height=28, units='in')
 print(p)
+warnings()
 dev.off()
