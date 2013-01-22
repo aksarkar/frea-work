a3853f84c6d9d6c19b4d15a8bca45ad10a079494
Abhishek Sarkar <aksarkar@mit.edu>
2013-01-22 13:16:20 -0500

    Improve scannability of plot

--- ld/block-size/plot.R
+++ ld/block-size/plot.R
@@ -3,9 +3,10 @@ library(ggplot2)
 
 args <- commandArgs(TRUE)
 D <- read.table(args[1], header=FALSE, sep=' ')
-p <- (qplot(data=D, x=V2, y=V3, color=factor(V1), xlab='p-value',
-            ylab='average number of SNPs in LD (R^2 > .2)', geom='line') +
-      scale_color_brewer(palette='Set2') +
+p <- (qplot(data=D, x=V2, y=V3, color=factor(V1), xlab='P-value (binned)',
+            ylab='Average number of SNPs in LD (R^2 > .2)', geom='line',
+            size=I(.5)) +
+      scale_color_brewer(palette='Set2', name='Phenotype') +
       theme_bw())
 CairoPDF(file='out.pdf')
 print(p)
