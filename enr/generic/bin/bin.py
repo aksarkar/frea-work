1d1016aaf0d5ccc4338d8145210fc2acc1e1b971
Abhishek Sarkar <aksarkar@mit.edu>
2013-03-26 20:49:15 -0400

    Don't write out the header to simplify postprocessing
    This way we don't have to delete the line from each of the individual output
    files.

--- enr/generic/bin/bin.py
+++ enr/generic/bin/bin.py
@@ -16,7 +16,6 @@ binsize = int(sys.argv[4])
 data = [int(x) for x in sys.stdin]
 total = data.count(1)
 w = csv.writer(sys.stdout)
-w.writerow(['total', 'phenotype', 'celltype', 'feature', 'count', 'expected'])
 count = 0
 for i in range(0, len(data), binsize):
     count += data[i:i+binsize].count(1)
