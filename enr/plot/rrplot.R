05dd2792d98d7c428f18720c3716f2c3dbaac5bb
Abhishek Sarkar <aksarkar@mit.edu>
2013-03-26 20:48:17 -0400

    Parameterize according to y-value computation, axis name, and zero point.
    Want to compare cumulative deviation, fold enrichment, and z-score based on
    negative controls.

--- enr/plot/rrplot.R
+++ enr/plot/rrplot.R
@@ -2,66 +2,70 @@
 # Author: Abhishek Sarkar <aksarkar@mit.edu>
 library(directlabels)
 library(ggplot2)
+library(grid)
 library(plyr)
 library(Cairo)
 
-filter <- function(d, ...) {
-  return(head(d[order(d$y, decreasing=TRUE),], n=10))
+filter <- function(n) {
+  function(d, ...) {
+    head(d[order(d$y, decreasing=TRUE),], n=n)
+  }
 }
 
-my.bumpup <- function (d, ...) {
+my.bumpup <- function(d, ...) {
   if (nrow(d) > 1) {
-    return bumpup(d)
+    return(bumpup(d))
   }
   else {
     return(d)
   }
 }
 
-scale_cell_type <-
-  scale_color_manual(name='Cell type',
-                     values=c('H1' = '#3e8f54', 'HUVEC' = '#e2560f',
-                       'GM12878' = '#9e320e', 'HepG2' = '#1182bb',
-                       'HMEC' = '#3fe7d2', 'K562' = '#3c11bc',
-                       'NHEK' = '#bb1d94', 'HSMM' = '#e0c31b',
-                       'NHLF' = '#806cd3'))
-
-scale_state_aggregate <-
-  scale_color_manual(name='State (aggregate)',
-                     values=c('promoter' = '#ff0000', 'enhancer' = '#faca00',
-                       'insulator' = '#09befe', 'transcribed' = '#00b050',
-                       'repressed' = '#7f7f7f', 'other' = 'black',
-                       'Poly-A+ RNA-Seq' = '#005c1f', 'diffexpr' = '#0060a0'))
-
-dev <- function(X, binsize=1000) {
-  return(data.frame(X$total, (X$count - X$expected) / max(X$count)))
+dev <- function(X) {
+  ddply(X, .(phenotype, feature, celltype), transform,
+        y=(count - expected) / max(count))
 }
 
-rrplot <- function(X) {
-  return(ggplot(X, aes(x=total, y=dev, color=celltype)) +
+fold <- function(X) {
+  transform(X, y=count / (1 + expected))
+}
+
+z.score <- function(X, Y, f) {
+  transform(merge(f(X),
+                  ddply(f(Y), .(total, phenotype, celltype, feature),
+                        function(x) c(m=mean(x$y), s=sd(x$y)))),
+            y=(y - m) / s)
+}
+
+rrplot <- function(X, name, zero, cutoff=30000) {
+  stopifnot(is.data.frame(X))
+  stopifnot(is.numeric(zero))
+  stopifnot(cutoff > 0)
+  return(ggplot(X[X$total <= cutoff, ],
+                aes(x=total, y=y, color=celltype)) +
          geom_line(size=I(.25)) +
-         geom_hline(yintercept=0, color='black') +
+         geom_hline(yintercept=zero, color='black', size=I(.25)) +
          geom_dl(aes(label=celltype),
-                 method=list(cex=.35, 'last.points', 'filter', 'my.bumpup')) +
-         scale_x_continuous(name='SNPs ranked by beta',
-                            limits=c(0, 200000),
-                            breaks=c(0, 50000, 100000, 150000)) +
-         scale_y_continuous(name='Cumulative deviation from expected count') +
+                 method=list(cex=.8, 'last.points', filter(10), 'my.bumpup')) +
+         facet_grid(phenotype ~ feature, scale='free') +
+         scale_x_continuous(name='Rank', limits=c(0, 1.6 * cutoff)) +
+         scale_y_continuous(name=name) +
          theme_bw() +
          theme(strip.background=element_blank(),
                legend.position="none"))
 }
 
 args <- commandArgs(TRUE)
-D <- read.csv(args[1])
-E <- ddply(D, .(phenotype, feature, celltype), dev)
-rm(D)
-colnames(E) <- c('phenotype', 'feature', 'celltype', 'total', 'dev')
-p <- (rrplot(subset(E, total < 150000)) +
-      facet_grid(phenotype ~ feature, scale='free'))
-write.csv(E, file='rrplot.csv', quote=FALSE, row.names=FALSE)
-Cairo(type='pdf', file='rrplot.pdf', dpi=96, width=4 * length(table(E$feature)),
-      height=4 * length(table(E$phenotype)), units='in')
-print(p)
+D <- read.csv(args[1], header=FALSE)
+colnames(D) <- c('total', 'phenotype', 'celltype', 'feature', 'count', 'expected')
+E <- read.csv(args[2], header=FALSE)
+colnames(E) <- c(colnames(D), 'rep')
+h <- 15
+w <- 1.6 * h
+Cairo(file=gsub(".in", ".pdf", args[1]), type='pdf', width=w, height=h, units='cm', dpi='auto')
+## rrplot(dev(D), 'Normalized deviation', 0)
+rrplot(fold(D), 'Fold enrichment', 1)
+## rrplot(z.score(D, E, dev), 'Z-score (normalized deviation)', 0)
+rrplot(z.score(D, E, fold), 'Z-score (fold enrichment)', 0)
 warnings()
 dev.off()
