0946a26115f500c7cb9952ff52d3d31dba86b45e
Abhishek Sarkar <aksarkar@mit.edu>
2013-03-26 20:46:45 -0400

    Superimpose hits on SNP density
    We use a hack to plot density as the background: plot square points at the
    midpoint of the interval (= midpoint of the transformed box)

--- enr/plot/hilbert.R
+++ enr/plot/hilbert.R
@@ -27,34 +27,41 @@ x.to.uv <- function(x, n) {
   c(u=u, v=v)
 }
 
-hilbert.plot <- function(d) {
-  n <- max(ddply(d, .(chr), function(x) {
-    bitShiftL(1, ceiling(log(sqrt(max(x$start))) / log(2)))
-  })$V1)
+hilbert.plot <- function(d, e) {
+  n <- 16384
   to.uv <- function(x) {
-    cbind(data.frame(score=x$score), ldply(x$start, x.to.uv, n))
+    ldply(x$start, x.to.uv, n)
   }
-  d <- ddply(d, .(chr), to.uv)
-  return(ggplot(d, aes(x=u, y=v, color=score)) +
-         geom_point(size=I(1)) +
-         scale_x_continuous(expand=c(0, 0)) +
-         scale_y_continuous(expand=c(0, 0)) +
-         scale_color_gradient(name='Beta', low='yellow', high='red') +
-         expand_limits(x=0, y=0) +
+  d <- ddply(d, .(chr, phenotype), to.uv)
+  e <- ddply(e, .(chr, score), to.uv)
+  return(ggplot(d, aes(x=u, y=v)) +
+         geom_point(data=e, aes(alpha=score), size=I(1), shape=I(15)) +
+         geom_point(aes(color=phenotype, shape=phenotype), size=I(1)) +
+         scale_x_continuous(limits=c(0, n), expand=c(0, 0)) +
+         scale_y_continuous(limits=c(0, n), expand=c(0, 0)) +
+         scale_color_manual(name="Phenotype", values=c("#cc0000", "#0000cc")) +
+         scale_shape_manual(values=c(3, 4), guide=FALSE) +
+         scale_alpha(name="SNP density", range=c(0, .5)) +
          coord_equal() +
-         facet_wrap(~ chr) +
+         facet_wrap(~ chr, nrow=3) +
          theme_bw() + 
          theme(text=element_text(size=8),
                axis.text.x=element_text(angle=90, hjust=1),
                axis.ticks=element_blank(),
                axis.title=element_blank(),
+               legend.position='bottom',
                strip.background=element_blank()))
 }
 
 args <- commandArgs(TRUE)
 d <- read.delim(args[1], header=FALSE)
-colnames(d) <- c('chr', 'start', 'end', 'name', 'score')
-p <- hilbert.plot(d)
-pdf(width=12, height=12)
+colnames(d) <- c('chr', 'start', 'end', 'name', 'score', 'phenotype', 'feature', 'celltype')
+e <- read.delim(args[2], header=FALSE)
+colnames(e) <- c('chr', 'start', 'end', 'name', 'score')
+d$chr <- factor(d$chr,
+                levels=c(vapply(seq(1, 22), function(x) sprintf('chr%d', x), ''),
+                  'chrX'))
+p <- hilbert.plot(d, e)
+pdf(file=sub(".in", ".pdf", args[1]), width=11, height=8.5)
 print(p)
 dev.off()
