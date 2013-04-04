41035a693f4a083793baa5b94a284c4a2ff518d6
Abhishek Sarkar <aksarkar@mit.edu>
2013-04-04 13:17:15 -0400

    Use chromsweep to save on memory

--- ld/1kg/expand.sh
+++ ld/1kg/expand.sh
@@ -8,7 +8,7 @@
 # Author: Abhishek Sarkar <aksarkar@mit.edu>
 set -e
 ld=/broad/compbio/aksarkar/ld/1kg
-bedtools intersect -a stdin -b $ld/haploreg.bed -wb | \
+bedtools intersect -a stdin -b $ld/haploreg_b137_chromsweep.bed -wb -sorted | \
     awk '{print $9, $5}' | \
     python $HOME/code/ld/1kg/expand.py $ld/ceu-index.txt $ld/CEU.txt $1 | \
     sort -k1 | \
