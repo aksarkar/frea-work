22a3ad69c7b6d89a467d0aa094cd812e3401d5c7
Abhishek Sarkar <aksarkar@mit.edu>
2013-05-13 18:27:32 -0400

    Add options to filter or subtract variants from foreground and background

--- enr/generic/ranksum.sh
+++ enr/generic/ranksum.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 # Test enrichment of markers for functional elements
-# Usage: ranksum.sh MARKERS FEATURES
+# Usage: ranksum.sh MARKERS FEATURES [FILTER MASK]
 # Author: Abhishek Sarkar
 set -e
 phenotype=$(basename $1 | sed "s/.bed.gz//")
 a=$(echo $2 | sed -e "s#.*features/##" -e "s/.bed.gz//")
 feature=$(echo $a | cut -d/ -f1)
 celltype=$(echo $a | cut -d/ -f2-)
-echo "$phenotype,$feature,$celltype,"
+if [[ $3 == "intersect" ]]
+then
+    mask="+$(basename $4 | sed s/.bed.gz//)"
+elif [[ $3 == "subtract" ]]
+then
+    mask="-$(basename $4 | sed s/.bed.gz//)"
+else
+    mask=""
+fi
+echo -n "$phenotype,$feature$mask,$celltype,"
 t=$(mktemp -p /broad/hptmp/aksarkar)
-bedtools intersect -a $1 -b $2 -c | \
-    bedtools sort -i stdin | \
+bedtools intersect -a $1 -b $2 -sorted -c | \
+{
+    if [[ ! -z $3 ]]
+    then
+        bedtools $3 -a stdin -b $4 $([[ $3 = "intersect" ]] && echo -sorted)
+    else
+        cat
+    fi
+} | \
     cut -f5,6 >$t
 wc -l <$t | cat - $t | $HOME/code/test/exact/ranksum
 rm $t
