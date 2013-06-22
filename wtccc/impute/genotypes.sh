223ae64056316704914e47ea22d7c8d94075c630
Abhishek Sarkar <aksarkar@mit.edu>
2013-06-21 23:35:37 -0400

    Rewrite genotype lifting code to work with pmap

--- wtccc/impute/genotypes.sh
+++ wtccc/impute/genotypes.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
-script="python $HOME/code/wtccc/impute/genotypes.py"
-ebi=/broad/compbio/lward/GWAS/ebi_ega_downloads
-base=/broad/compbio/aksarkar/projects/impute
-diseases=(BD CAD CD HT RA T1D T2D)
-chr=({1..22} 24)
-for i in ${chr[@]}
-do
-    for d in ${diseases[@]}
-    do
-        in=$ebi/WTCCC_7_Diseases/$d/Affymetrix500K/Chiamo/OXSTATS_FORMAT
-        out=$base/genotypes/$(echo $d | tr '[A-Z]' '[a-z]')
-        echo "zcat $in/${d}_$(printf '%02d' $i)_chiamo.gz | $script $base/affy/chr$i.bed | gzip >$out/$i.gen.gz"
-    done
-    echo "zcat $ebi/1958BC/WTCCC1_1958BC/Affymetrix500K/Chiamo/Oxstat_format/58C_$(printf '%02d' $i)_chiamo.gz | $script $base/affy/chr$i.bed | gzip >$base/genotypes/1958bc/$i.gen.gz"
-    echo "zcat $ebi/NBS/WTCCC1/Affymetrix500K/Chiamo/Oxstat_format/NBS_$(printf '%02d' $i)_chiamo.gz | $script $base/affy/chr$i.bed | gzip >$base/genotypes/nbs/$i.gen.gz"
-done
+# Usage: genotypes.sh GEN LOOKUP LEGEND
+set -e
+export LC_ALL=C
+zcat $1 | \
+    sort -k1 | \
+    join $2 - | \
+    cut -d' ' -f1-3,6- | \
+    sort -k3g | \
+    python $HOME/code/wtccc/impute/orient.py $3 | \
+    gzip
