3585282ad2ef9b48d27d93d02424616e2a77ca7f
Abhishek Sarkar <aksarkar@mit.edu>
2013-09-15 13:02:49 -0400

    Don't try to guess the strand
    Let IMPUTE2 do it instead

--- wtccc/impute/genotypes.sh
+++ wtccc/impute/genotypes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Usage: genotypes.sh GEN LOOKUP LEGEND
+# Usage: genotypes.sh GEN LOOKUP
 set -e
 export LC_ALL=C
 zcat $1 | \
@@ -7,5 +7,4 @@ zcat $1 | \
     join $2 - | \
     cut -d' ' -f1-3,6- | \
     sort -k3g | \
-    python $HOME/code/wtccc/impute/orient.py $3 | \
     gzip
