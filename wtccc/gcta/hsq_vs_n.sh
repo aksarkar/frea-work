cf5f9f834045a3afb594c30ed27ef46ff95d670e
Abhishek Sarkar <aksarkar@mit.edu>
2013-12-27 19:28:57 -0500

    Rewrite to work with pmap
    Don't require human intervention to generate training/test sets

--- wtccc/gcta/hsq_vs_n.sh
+++ wtccc/gcta/hsq_vs_n.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 pheno=${1?"missing phenotype"}
+p=$(basename $pheno .txt)
 fold=${2?"missing fold"}
-n="100 500 1000 5000 10000 50000 100000 271428"
 qc=/broad/compbio/aksarkar/projects/gwas/wtccc1/results/gcta/2013-12-06/qc
-plink --bfile $qc --remove test-sets/$fold.txt --pheno $pheno --logistic --out rankings/$fold &>/dev/null
-sort -k9g rankings/$fold.assoc.logistic | awk 'NR > 1 {print $2}' >rankings/$fold.txt
-parallel -j1 'gcta64 --thread-num 16 --bfile '$qc' --keep test-sets/{1}.txt --extract <(head -n{2} rankings/{1}.txt) --maf .01 --make-grm-bin --out grms/{1}.{2} &>grms/{1}.{2}.log' ::: $fold ::: $n
-parallel -j1 'gcta64 --thread-num 16 --grm-bin grms/{1}.{2} --grm-cutoff .05 --reml --pheno '$pheno' --prevalence .005 --out hsq/{1}.{2} &>hsq/{1}.{2}.log' ::: $fold ::: $n
+prune=/broad/compbio/aksarkar/projects/gwas/wtccc1/results/gcta/2013-12-06/h2g-all-samples/prune.txt
+python $HOME/code/wtccc/gcta/testsets.py <$pheno >test-sets/$p.$fold.txt
+plink --bfile $qc --remove test-sets/$p.$fold.txt --pheno $pheno --1 --logistic --out rankings/$p.$fold &>/dev/null
+sort -k9g rankings/$p.$fold.assoc.logistic | awk 'NR > 1 {print $2}' >rankings/$p.$fold.txt
+n="1000 2000 3000 4000 5000 7500 10000 25000 50000 100000 271428"
+parallel -j1 "gcta64 --thread-num 16 --bfile $qc --keep test-sets/$p.$fold.txt --remove $prune --extract "'<('"head -n{} rankings/$p.$fold.txt"')'" --make-grm --out grms/$p.$fold.{} &>grms/$p.$fold.{}.log" ::: $n
+parallel -j1 "gcta64 --thread-num 16 --grm grms/$p.$fold.{} --reml --pheno $pheno --prevalence .005 --out hsq/$p.$fold.{} &>hsq/$p.$fold.{}.log" ::: $n
--- wtccc/gcta/testsets.py
+++ wtccc/gcta/testsets.py
@@ -4,10 +4,7 @@ import sys
 data = [line.split() for line in sys.stdin]
 cases = [(f, i) for f, i, c in data if c == '1']
 controls = [(f, i) for f, i, c in data if c == '0']
-k = int(sys.argv[1])
-for i in range(k):
-    with open('{}.txt'.format(i), 'w') as out:
-        for f, i in random.sample(cases, len(cases) // 2):
-            print(f, i, 1, file=out)
-        for f, i in random.sample(controls, len(controls) // 2):
-            print(f, i, 0, file=out)
+for f, i in random.sample(cases, len(cases) // 2):
+    print(f, i, 1)
+for f, i in random.sample(controls, len(controls) // 2):
+    print(f, i, 0)
