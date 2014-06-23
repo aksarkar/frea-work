1cba8f352958ba6f61e45ec41e806ad4fd844652
Abhishek Sarkar <aksarkar@mit.edu>
2014-06-23 12:31:31 -0400

    Only impute in windows with enough typed SNPs

--- impute2/pre/breaks.py
+++ impute2/pre/breaks.py
@@ -8,6 +8,6 @@ next(data)
 pos = (int(row[2]) for row in data)
 windows = (k for k, g in
            itertools.groupby(pos, key=lambda x: x // window_size)
-           if list(g))
+           if len(list(g)) > 100)
 for i, k in enumerate(windows):
     print('{:02d} {} {}'.format(i, k * window_size, (k + 1) * window_size))
