9b761cfba8809cf6d4de8afb2114e528785f6548
Abhishek Sarkar <aksarkar@mit.edu>
2014-06-23 12:35:59 -0400

    Copy GCTA implementation of GRM pruning
    We need the list of sample IDs which were removed

--- wtccc/gcta/prune_grm.py
+++ wtccc/gcta/prune_grm.py
@@ -1,19 +1,28 @@
-import collections
+"""Prune related pairs of individuals from the GRM
+
+Usage: python prune_grm.py FAM
+
+Based on the implementation in GCTA 1.20. Expects (ID1, ID2, N,
+relatedness) tuples on stdin. Writes individuals to be excluded on
+stdout.
+
+Author: Abhishek Sarkar <aksarkar@mit.edu>
+
+"""
+import itertools
 import sys
 
 with open(sys.argv[1]) as f:
     samples = [line.strip() for line in f]
 
-pairs = collections.defaultdict(set)
 data = (line.split() for line in sys.stdin)
-for i, j in data:
-    pairs[i].add(j)
-    pairs[j].add(i)
-while pairs:
-    best = sorted(pairs.keys(), key=lambda x: len(pairs[x]), reverse=True)[0]
-    print(samples[int(best) - 1])
-    pairs.pop(best)
-    for ind in pairs:
-        if best in pairs[ind]:
-            pairs[ind].remove(best)
-    pairs = {k: v for k, v in pairs.items() if v}
+grm = ((int(a), int(b), float(k)) for a, b, _, k in data)
+A, B = [list(x) for x in zip(*[(a, b) for a, b, k in grm
+                               if a > b and k > .025])]
+C = {k: len(list(g)) for k, g in itertools.groupby(sorted(A + B))}
+for i in range(len(A)):
+    if C[A[i]] < C[B[i]]:
+        A[i], B[i] = B[i], A[i]
+prune = (k for k, _ in itertools.groupby(sorted(A)))
+for p in prune:
+    print(samples[p - 1])
