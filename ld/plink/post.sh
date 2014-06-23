3769861e42ca4dd7c5c8a579e50c45977251b064
Abhishek Sarkar <aksarkar@mit.edu>
2014-06-23 12:33:23 -0400

    Postprocess PLINK pairwise LD

--- ld/plink/post.sh
+++ ld/plink/post.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
+set -e
 export LC_ALL=C
-awk -f $HOME/code/ld/plink/post.awk | \
-    sort -k2 | \
-    join - t1d.txt -12 -24 -t$'\t' -o '2.1 2.2 2.3 1.1 2.5' | \
-    bedtools sort
+join -12 -24 -t$'\t' -o '2.1 2.2 2.3 1.1 2.5' <(zcat $1/*.gz | grep -v CHR | awk -vOFS='\t' '{split($6, snps, "|"); for (i in snps) {print $1":"$2"-"$3, snps[i]}}' | sort -k2) <(zcat $2 | sort -k4) | bedtools sort | gzip
