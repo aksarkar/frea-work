8fcab61ca95d8970c4ed7090c3b146293f187827
Abhishek Sarkar <aksarkar@mit.edu>
2014-06-23 12:06:46 -0400

    Deal with multiple output formats, ontologies
    Output format depends on the background set (whole genome vs. regions)

--- enr/great/heatmap.R
+++ enr/great/heatmap.R
@@ -10,34 +10,36 @@ cluster.labels <- function(m) {
 }
 
 heatmap <- function(X, limits=NULL) {
-  if (is.null(limits)) {
-    limits <- c(0, max(X$fold))
-  }
-  print(ggplot(X, aes(x=cell, y=path, fill=fold)) +
-      geom_tile() +
-      scale_fill_gradient(name='Fold enrichment', low='white', high='red', limits=limits) +
-      scale_x_discrete(name='Cell type', limits=cell.labels) +
-      scale_y_discrete(name='GO biological process', limits=path.labels[path.labels %in% X$path]) +
-      coord_equal() +
-      theme_bw() +
-      theme(text=element_text(size=6),
-            axis.text.x=element_text(angle=-90, hjust=0, vjust=.5),
-            axis.ticks=element_blank(),
-            panel.grid=element_blank()))
+    print(ggplot(X, aes(x=path, y=cell, fill=fold)) +
+          geom_tile() +
+          scale_fill_gradient(name='Fold enrichment', low='white', high='red') +
+          scale_x_discrete(name=X$V4[1]) +
+          scale_y_discrete(name='Cell type') +
+          coord_fixed(ratio=1) +
+          coord_flip() +
+          theme_bw() +
+          theme(legend.position='bottom',
+                text=element_text(size=10),
+                axis.text.x=element_text(angle=-90,hjust=0,vjust=.5),
+                ## axis.text=element_blank(),
+                axis.ticks=element_blank(),
+                panel.grid=element_blank()))
 }
 
 args <- commandArgs(TRUE)
-d <- read.delim(args[1], header=0)
+d <- read.delim(gzfile(args[1]), header=0)
+load(args[2])
 d$cell <- strtrim(d$V1, 40)
 d$path <- strtrim(d$V2, 50)
 d$fold <- d$V3
-m <- cast(d, cell~path, value='fold')
-cell.labels <- cluster.labels(m)
+m <- cast(d, cell~path, value='fold', fun.aggregate=length)
+d$cell <- factor(d$cell, levels=vapply(sample_info$name[order(sample_info$position)], function(x) {gsub(' ', '_', x)}, ""))
 path.h <- hclust(dist(t(m)))
-path.labels <- path.h$labels[path.h$order]
-d$clust <- cutree(path.h, k=16)[d$path]
+d$path <- factor(d$path, levels=path.h$labels[rev(path.h$order)])
+d$clust <- cutree(path.h, k=64)[d$path]
 limits=c(0, max(d$fold))
 
-svg()
-d_ply(d, .(clust), heatmap)
+pdf(height=17, width=11)
+## d_ply(d, .(clust), heatmap)
+heatmap(d)
 dev.off()
--- enr/great/post.awk
+++ enr/great/post.awk
@@ -1,17 +1,25 @@
 BEGIN {
     FS="\t"
     OFS="\t"
-    if (c == "") {
-        c=.01
-    }
 }
 
 NR == 1 {
     f=FILENAME
     sub(/^.*\//, "", f)
     sub(/\..*$/, "", f)
+    print "cluster", "term", "fold", "p", "q", "genes"
 }
 
-$1 == "GO Biological Process" && $7 < c && $16 < c && $8 > 1 {
-    print f, $3, $8
+NR == 5 {
+    sub(/^# /, "", $0)
+    for (i = 1; i < NF; i++) {
+        header[$i] = i
+    }
+}
+
+(NR > 5 && 
+ $header["Ontology"] == "GO Biological Process" && 
+ $header["HyperFdrQ"] < 1e-4 && 
+ $header["RegionFoldEnrich"] > 1) {
+    print f, $header["Desc"], $header["RegionFoldEnrich"], $header["HyperP"], $header["HyperFdrQ"], $header["FgGeneNames"]
 }
