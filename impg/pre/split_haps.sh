987c7bf61c27552f0a249c03536c3dbd2ff90c15
Abhishek Sarkar <aksarkar@mit.edu>
2014-07-27 00:40:48 -0400

    Use absolute paths for now

--- impg/pre/split_haps.sh
+++ impg/pre/split_haps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export LC_ALL=C
-echo "SNP $(awk 'NR > 1 {print $1; print $1}' ../../../data/1kg/ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample | tr '\n' ' ')"
+echo "SNP $(awk 'NR > 1 {print $1; print $1}' /broad/compbio/aksarkar/projects/impute2/1kg/ALL.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.sample | tr '\n' ' ')"
 sort -k1 $1 | join - $2 | sort -k2g | cut -d' ' -f1,5-
