b58b1a804cc1584398d1b24cea5fc08cbe770d78
Abhishek Sarkar <aksarkar@mit.edu>
2014-08-18 01:11:17 -0400

    Update script name

--- enr/generic/matched/joblist.sh
+++ enr/generic/matched/joblist.sh
@@ -4,4 +4,4 @@ cutoffs=${1?"missing cutoffs"}
 shift
 table=${1?"missing table"}
 shift
-parallel -j1 -C" " 'parallel --dry-run -I[] '"'"'echo ">>>" $(basename {1}) $(basename [] .bed.gz) $(basename {2} .bed.gz) $(bedtools intersect -a {2} -b [] -sorted -c | python $HOME/code/enr/generic/matched/fisher.py '$table' {3} 10000)'"'"' :::: <(find {1} -name "*.bed.gz")' ::: $* :::: $cutoffs
+parallel -j1 -C" " 'parallel --dry-run -I[] '"'"'echo ">>>" $(basename {1}) $(basename [] .bed.gz) $(basename {2} .bed.gz) $(bedtools intersect -a {2} -b [] -sorted -c | python $HOME/code/enr/generic/matched/test.py '$table' {3} 10000)'"'"' :::: <(find {1} -name "*.bed.gz")' ::: $* :::: $cutoffs
