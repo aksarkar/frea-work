2403d6edf9a0f90fdb4664dfb67c7312177aeee9
Abhishek Sarkar <aksarkar@mit.edu>
2014-09-20 12:58:08 -0400

    Update data path

--- impg/pre/haps.sh
+++ impg/pre/haps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Usage: haps.sh CHROMOSOME
 export LC_ALL=C
-prefix=/broad/compbio/aksarkar/projects/impute2/1kg/ALL.chr${1}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing
+prefix=/broad/compbio/aksarkar/data/1kg/ALL.chr${1}.integrated_phase1_v3.20101123.snps_indels_svs.genotypes.nosing
 paste -d' ' <(zcat $prefix.legend.gz | awk 'NR > 1 {print $1}') <(zcat $prefix.haplotypes.gz | tr 01 12 | tr -d ' ') | sort -k1
