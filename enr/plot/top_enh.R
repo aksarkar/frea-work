a80b3a0a316204b88d323757c24371881278b6be
Abhishek Sarkar <aksarkar@mit.edu>
2014-09-20 13:00:27 -0400

    Clean up plot

--- enr/plot/top_enh.R
+++ enr/plot/top_enh.R
@@ -1,23 +1,23 @@
 library(ggplot2)
 library(grid)
 library(reshape)
+library(scales)
 library(Cairo)
 
+source('~/code/enr/plot/theme_nature.R')
+
 args <- commandArgs(TRUE)
-d <- read.table(args[1], header=FALSE, sep=' ')
-h <- hclust(dist(as.matrix(cast(d, V1 ~ V2, value='V3'))))
-labels <- unique(d$V1)[h$order]
+d <- read.delim(args[1], header=FALSE)
 p <- (ggplot(d, aes(x=V1, y=V2, label=V3, fill=V3)) +
       geom_tile() +
-      geom_text(size=2.5) +
+      geom_text(size=(5 / ggplot2:::.pt)) +
       coord_equal() +
       scale_fill_gradient(low='white', high='red', guide='none') +
-      scale_x_discrete(name='Reference epigenome', limits=labels) +
-      scale_y_discrete(name='Reference epigenome', limits=rev(labels)) +
-      theme_bw() +
-      theme(text=element_text(size=8),
-            axis.text.x=element_text(angle=-45, hjust=0, vjust=1),
-            plot.margin=unit(c(0,1,0,0), "in")))
-Cairo(type='pdf', file=sub('.txt$', '.pdf', args[1]), dpi=96, height=6, width=8, units='in')
+      scale_y_discrete(limits=rev(levels(d$V1))) +
+      theme_nature +
+      theme(text=element_text(size=5),
+            axis.title=element_blank(),
+            axis.text.x=element_text(angle=45, hjust=1, vjust=1)))
+Cairo(type='pdf', file=sub('.txt$', '.pdf', args[1]), dpi='auto', height=89, width=89, units='mm')
 print(p)
 dev.off()
