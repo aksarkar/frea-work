77c1e3277854fd14773aa010102089831df6fa3f
Abhishek Sarkar <aksarkar@mit.edu>
2015-01-27 19:25:43 -0500

    Catch DAVID server errors

--- enr/david/david.py
+++ enr/david/david.py
@@ -12,13 +12,12 @@ with open(sys.argv[2]) as f:
     bg = ','.join(line.strip() for line in f)
 if not ids:
     sys.exit(0)
-david = suds.client.Client('http://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService?wsdl').service
+david = suds.client.Client('http://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService?wsdl', faults=False).service
 david.authenticate(b'aksarkar@mit.edu')
 epigenome, _ = os.path.splitext(os.path.basename(sys.argv[1]))
 david.addList(ids, 'ENSEMBL_GENE_ID', epigenome, 0)
 david.addList(bg, 'ENSEMBL_GENE_ID', "back", 1)
-clusters = david.getTermClusterReport(4, 4, 4, .5, 35)
-w = csv.writer(sys.stdout)
+_, clusters = david.getTermClusterReport(4, 4, 4, .5, 35)
 for i, cluster in enumerate(clusters):
     for term in cluster.simpleChartRecords:
-        w.writerow([i, cluster.score, term.categoryName, term.termName, '{:.3g}'.format(term.ease), '{:.3g}'.format(term.foldEnrichment)])
+        print(i, cluster.score, term.categoryName, term.termName, '{:.3g}'.format(term.foldEnrichment), '{:.3g}'.format(term.ease), '{:.3g}'.format(term.afdr), term.geneIds, sep='\t')
