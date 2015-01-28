27ffd9aa74c0d5d78d2e33207027fd3d3039064f
Abhishek Sarkar <aksarkar@mit.edu>
2015-01-27 19:27:32 -0500

    Handle SIGPIPE

--- util/multimerge.py
+++ util/multimerge.py
@@ -1,16 +1,19 @@
 import gzip
 import heapq
+import signal
 import sys
+
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
 def bed(f):
     for line in f:
-        chr_, start, end, *_ = line.split()
-        yield chr_, int(start), int(end)
+        chr_, start, end, *rest = line.split()
+        yield [chr_, int(start), int(end)] + rest
 
 files = [gzip.open(f) for f in sys.argv[1:]]
 parsed = [(str(l, 'utf-8') for l in f) for f in files]
 intervals = [bed(f) for f in parsed]
-for c, s, e in heapq.merge(*intervals):
-    print(c, s, e, sep='\t')
+for entry in heapq.merge(*intervals):
+    print(*entry, sep='\t')
 for f in files:
     f.close()
