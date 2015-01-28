5c983f0cbdc3971727050e0498e1dd09434d5563
Abhishek Sarkar <aksarkar@mit.edu>
2015-01-27 19:30:14 -0500

    Compute ticks for RR plots

--- enr/generic/bin/ticks.sh
+++ enr/generic/bin/ticks.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 set -u
-zcat $1 | sort -k4,4 -k5,5gr | awk -v op=weight -f ~/code/ld/union.awk | sort -k1gr | awk 'NR == 100 {thresh=int($1)} $1 < thresh {print thresh, NR; thresh -= 1}'
+zcat $1 | sort -k4,4 -k5,5gr | awk -v op=weight -f $HOME/code/ld/union.awk | sort -k1gr | awk 'BEGIN {thresh = 10} $1 < thresh {if (NR > 100) {print thresh, NR} thresh -= 1}'
