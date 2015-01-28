af8df0df9af42e3e2434ca1927c8e06023163861
Abhishek Sarkar <aksarkar@mit.edu>
2015-01-27 19:05:59 -0500

    Add documentation

--- ld/1kg/expand.py
+++ ld/1kg/expand.py
@@ -1,10 +1,20 @@
-import csv
+"""Expand SNPs according to 1KG LD
+
+Usage: python expand.py TAGS
+
+Expects id1, id2 pairs on stdin. Writes pairs on stdout
+
+Author: Abhishek Sarkar <aksarkar@mit.edu>
+
+"""
+
 import sys
 
 with open(sys.argv[1]) as f:
     tags = set(line.strip() for line in f)
 
-for row in csv.reader(sys.stdin, delimiter=' '):
+data = (line.split() for line in sys.stdin)
+for row in data:
     if row[0] in tags:
         print(row[0], row[1])
     elif row[1] in tags:
