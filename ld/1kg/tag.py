8c64320997f0856b8768e356b7279cfdd3a481f2
Abhishek Sarkar <aksarkar@mit.edu>
2015-09-18 21:32:05 -0400

    Move away from stdin to play nice with Makefile

--- ld/1kg/tag.py
+++ ld/1kg/tag.py
@@ -7,7 +7,7 @@ TAGS and SNPS are lists of IDs (one per line)
 Author: Abhishek Sarkar <aksarkar@mit.edu>
 
 """
-
+import gzip
 import sys
 
 with open(sys.argv[1]) as f:
@@ -16,14 +16,15 @@ with open(sys.argv[1]) as f:
 with open(sys.argv[2]) as f:
     snps = set(line.strip() for line in f)
 
-seen = set()
-ld = (line.split() for line in sys.stdin)
-for a, b, r, d in ld:
-    if a in tags and b in snps:
-        tags.remove(a)
-        seen.add(a)
-    elif b in tags and a in snps:
-        tags.remove(b)
-        seen.add(b)
-for s in seen:
-    print(s)
+with gzip.open(sys.argv[3], 'rt') as f:
+    seen = set()
+    ld = (line.split() for line in f)
+    for a, b, r, d in ld:
+        if a in tags and b in snps:
+            tags.remove(a)
+            seen.add(a)
+        elif b in tags and a in snps:
+            tags.remove(b)
+            seen.add(b)
+    for s in seen:
+        print(s)
