e144e29253880b94d3ee2b34b5fb992dfd5d5cc9
Abhishek Sarkar <aksarkar@mit.edu>
2015-09-18 21:24:55 -0400

    Make imputation postprocess play nicely with UGER

--- impute2/post/compress.sh
+++ impute2/post/compress.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+#$ -pe smp 8
 set -e
-find -type f -name "*.*" | parallel -X gzip
-mkdir $1
-mv *.* $1
+set -u
+cohort=${1?"missing cohort"}
+parallel --halt 1 "cat ${cohort}_{}_*.gen | gzip >${cohort}_{}.gen.gz && mv ${cohort}_{}.gen.gz /broad/hptmp/aksarkar/out" ::: $(seq -w 1 22)
+parallel --halt 1 -X rm ::: ${cohort}_* impute2.*
