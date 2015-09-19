a08228cbf17b6be3cb90307a0d03e87f96cc2479
Abhishek Sarkar <aksarkar@mit.edu>
2015-09-18 21:23:57 -0400

    Revert to testing by SNP

--- enr/generic/matched/test.sh
+++ enr/generic/matched/test.sh
@@ -7,6 +7,7 @@
 set -e
 set -u
 set -o pipefail
+export LC_ALL=C
 gwas=${1?"missing data"}
 shift
 thresh=${1?"missing thresh"}
@@ -15,5 +16,5 @@ table=${1?"missing table"}
 shift
 feature=${1?"missing feature"}
 shift
-result="$(bedtools intersect -sorted -nobuf -c -a $gwas -b $feature | sort -k4 | bedtools groupby -g 4 -c 6 -o max | join -24 - <(zcat /broad/compbio/aksarkar/projects/gwas/wtccc1/EC21/data/gwas-summary-stats/$(basename $gwas) | sort -k4) -o '2.1 2.2 2.3 2.4 2.5 1.2' | python $HOME/code/enr/generic/matched/test2.py $table $thresh 100000)"
-echo ">>> $(basename $gwas .bed.gz) $(basename $(dirname $feature)) $(basename $feature .bed.gz) $thresh $result"
+result="$(bedtools intersect -sorted -nobuf -c -a $gwas -b $feature | python $HOME/code/enr/generic/matched/test.py $table $thresh 10000)"
+echo "$(basename $gwas .bed.gz) $(basename $(dirname $feature)) $(basename $feature .bed.gz) $result"
