e27b9d6a082f314f6994102424f18c2f9d5462c1
Abhishek Sarkar <aksarkar@mit.edu>
2015-11-02 14:51:53 -0500

    Inverse-variance weight heritability estimates

--- wtccc/gcta/hsq_vs_n.sh
+++ wtccc/gcta/hsq_vs_n.sh
@@ -4,42 +4,53 @@
 # cross-validation setting
 #
 # Author: Abhishek Sarkar <aksarkar@mit.edu>
-set -u
 set -e
+set -u
 
-PREFIX=/broad/hptmp/aksarkar/haplotypes
-QC=$PREFIX/T1D-clean
+COHORT=("train" "test")
 CUTOFFS=(1000 2000 3000 4000 5000 7500 10000 20000 30000 40000 50000 100000 200000)
 
+CODE=$HOME/code/wtccc/gcta
 PCGC="java -jar $HOME/.local/src/PCGCRegression/java-pcgc/PCGCRegression.jar"
 PRINT='parallel -j1 echo :::'
 export LC_ALL=C
 
-pheno=${1?"missing phenotype"}
+PREFIX=${1?"missing prefix"}
+pheno=$PREFIX.pheno
 p=$(basename $pheno .pheno)
-fold=${2?"missing fold"}
-MAKE_GRM="gcta64-1.24 --thread-num ${NSLOTS:-1} --bfile $QC --keep $p.$fold.keep --make-grm"
-log=$p.$fold.log
-if [[ $pheno = "random" ]]
-then
-    pheno="$PREFIX/T1D.pheno"
-    awk '{print $1, $2}' $pheno >$p.$fold.test
-    cut -f2 $QC.bim | python $HOME/code/wtccc/gcta/random_order.py $fold >$p.$fold.txt
-else
-    python $HOME/code/wtccc/gcta/testset.py $fold <$pheno >$p.$fold.test
-    plink --memory 4096 --bfile $QC --remove $p.$fold.test --pheno $pheno --1 --logistic --out $p.$fold &>/dev/null
-    sort -k9g $p.$fold.assoc.logistic | awk 'NR > 1 {print $2}' >$p.$fold.txt
-fi
-comm -12 <(sed "s/\t/ /" $PREFIX/T1D.grm.id | sort) <(sort $p.$fold.test) >$p.$fold.keep
+seed=${2:-0}
+MAKE_GRM="gcta64-1.24 --thread-num ${NSLOTS:-1} --bfile $PREFIX-clean --make-grm"
+log=$p.$seed.log
 
-$PRINT $p.$fold{,.c} >$p.$fold.grms
-$PRINT "$PREFIX/T1D" "$PREFIX/T1D" >$p.$fold.subtractvecs
-$PRINT "grmlist=$p.$fold.grms" "subtractvecs=$p.$fold.subtractvecs" "phenos=$pheno" "covars=$PREFIX/T1D.eigenvec" "prevalence=0.005" >$p.$fold.params
-for n in ${CUTOFFS[@]}
+python -c "import sys; assert sys.version_info[0] == 3"
+python $CODE/testset.py $seed <$pheno >$p.keep
+comm -12 <(sed "s/\t/ /" $PREFIX.grm.id | sort) <(sort $p.keep) >$p.train
+comm -23 <(sed "s/\t/ /" $PREFIX.grm.id | sort) <(sort $p.keep) >$p.test
+
+$PRINT $p{,.c} >$p.grms
+$PRINT "$PREFIX" "$PREFIX" >$p.subtractvecs
+$PRINT "grmlist=$p.grms" "subtractvecs=$p.subtractvecs" "phenos=$pheno" "covars=$PREFIX.eigenvec" "prevalence=0.005" >$p.params
+for c in ${COHORT[@]}
 do
-    head -n$n $p.$fold.txt >$p.$fold.head
-    $MAKE_GRM --extract $p.$fold.head --out $p.$fold &>$log
-    $MAKE_GRM --exclude $p.$fold.head --out $p.$fold.c &>$log
-    $PCGC $p.$fold.params >$p.$fold.$n.hsq
+    if [[ seed == 0 ]]
+    then
+        plink --memory 4096 --bfile $PREFIX-clean --remove $p.$c --pheno $pheno --1 --logistic --out $p &>/dev/null
+        sort -k9g $p.assoc.logistic | awk 'NR > 1 {print $2}' >$p.txt
+    else
+        cut -f2 $PREFIX-clean.bim | python $CODE/random_order.py $seed >$p.txt
+    fi
+    for n in ${CUTOFFS[@]}
+    do
+        head -n$n $p.txt >$p.head
+        $MAKE_GRM --keep $p.$c --extract $p.head --out $p &>>$log
+        $MAKE_GRM --keep $p.$c --exclude $p.head --out $p.c &>>$log
+        if [[ seed == 0 ]]
+        then
+            hsq=$p.$n.$c.hsq
+        else
+            hsq=$p.$n.$c-$seed.hsq
+        fi            
+        $PCGC $p.params >$hsq 2>>$log
+    done
 done
-rm -f $p.$fold.{assoc.logistic,c.*,head,grm*,keep,params,subtractvecs,test,txt}
+rm -f $p.{assoc.logistic,c.*,head,grm*,keep,params,subtractvecs,train,test,txt}
--- wtccc/gcta/plot_hsq_vs_n.R
+++ wtccc/gcta/plot_hsq_vs_n.R
@@ -1,24 +1,38 @@
 library(ggplot2)
 library(grid)
+library(plyr)
 library(Cairo)
 
 source("~/code/enr/plot/theme_nature.R")
 
+inv_var <- function(x, label) {
+    x$w <- 1 / x$V4 ** 2
+    data.frame(mean=sum(x$V3 * x$w) / sum(x$w), se=sqrt(1 / sum(x$w)), order=rep(label))
+}
+
 args <- commandArgs(TRUE)
-d <- read.table(args[1], header=0, sep=' ')
-d$V3 <- d$V3 + rnorm(length(d$V3)) * 1e-7
-p <- (ggplot(d, aes(x=V2, y=V3)) +
-      geom_violin(aes(group=V2), size=I(.25)) +
-      stat_summary(fun.y=mean, geom='line', color='red', size=I(.25)) +
-      geom_hline(yintercept=as.numeric(args[2]), size=I(.25)) +
+hsq_by_n <- ddply(read.table(args[1], header=0, sep=' '), .(V1, V5), inv_var, "Observed")
+hsq_by_random_n <- ddply(read.table(args[2], header=0, sep=' '), .(V1, V5), inv_var, "Random")
+hsq <- rbind(hsq_by_n, hsq_by_random_n)
+all_sample_hsq <- read.table(args[3], header=0, sep=' ')
+p <- (ggplot(hsq, aes(x=V1, y=mean, ymin=mean - se, ymax=mean + se)) +
+      geom_line(aes(color=order), size=I(.25)) +
+      geom_ribbon(aes(fill=order), alpha=I(.1)) +
+      geom_hline(data=all_sample_hsq, aes(yintercept=V3), size=I(.25), linetype='dashed') +
+      geom_hline(yintercept=0, size=I(.25)) +
+      scale_x_log10(name='Top n tags', labels=unique(hsq$V1), breaks=unique(hsq$V1)) +
       scale_y_continuous(name=expression(h[g]^2)) +
-      scale_x_log10(name='Top n tags', labels=unique(d$V2), breaks=unique(d$V2)) +
+      scale_color_manual(values=c("Observed"="red", "Random"="gray50")) +
+      scale_fill_manual(values=c("Observed"="red", "Random"="black")) +
+      facet_wrap(~ V5, ncol=2, scales='free') +
       theme_nature +
       theme(axis.title.y=element_text(angle=0),
             axis.text=element_text(size=5),
             axis.text.x=element_text(angle=-90, hjust=0, vjust=.5),
+            legend.position="right",
+            panel.margin=unit(rep(5, 4), 'mm'),
             plot.margin=unit(c(1, 0, 0, 0), 'mm')
             ))
-Cairo(dpi='auto', file=sub('.txt', '.pdf', args[1]), height=37, type='pdf', units='mm', width=60)
+Cairo(dpi='auto', file=sub('.txt', '.pdf', args[1]), height=190, type='pdf', units='mm', width=160)
 print(p)
 dev.off()
