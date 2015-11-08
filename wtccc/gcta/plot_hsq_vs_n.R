26d47981db21e3a7ffd559be7cbb676183f4f5ac
Abhishek Sarkar <aksarkar@mit.edu>
2015-11-08 10:42:49 -0500

    Plot inverse-variance weighted h_g^2

--- wtccc/gcta/plot_hsq_vs_n.R
+++ wtccc/gcta/plot_hsq_vs_n.R
@@ -11,25 +11,26 @@ inv_var <- function(x, label) {
 }
 
 args <- commandArgs(TRUE)
-hsq_by_n <- ddply(read.table(args[1], header=0, sep=' '), .(V1, V5), inv_var, "Observed")
-hsq_by_random_n <- ddply(read.table(args[2], header=0, sep=' '), .(V1, V5), inv_var, "Random")
+hsq_by_n <- ddply(read.table(args[1], header=0, sep=' '), .(V2, V5), inv_var, "Observed")
+hsq_by_random_n <- ddply(read.table(args[2], header=0, sep=' '), .(V2, V5), inv_var, "Random")
 hsq <- rbind(hsq_by_n, hsq_by_random_n)
 all_sample_hsq <- read.table(args[3], header=0, sep=' ')
-p <- (ggplot(hsq, aes(x=V1, y=mean, ymin=mean - se, ymax=mean + se, color=order)) +
-      geom_line(size=I(.25)) +
-      geom_ribbon(alpha=I(.25)) +
+p <- (ggplot(hsq, aes(x=V2, y=mean, ymin=mean - se, ymax=mean + se)) +
+      geom_line(size=I(.25), aes(color=order)) +
+      geom_ribbon(alpha=I(.2), aes(fill=order)) +
       geom_hline(data=all_sample_hsq, aes(yintercept=V3), size=I(.25), linetype='dashed') +
-      scale_x_log10(name='Top n tags', labels=unique(hsq$V1), breaks=unique(hsq$V1)) +
+      geom_hline(yintercept=0, size=I(.25)) +
+      scale_x_log10(name='Top n tags', labels=unique(hsq$V2), breaks=unique(hsq$V2)) +
       scale_y_continuous(name=expression(h[g]^2)) +
-      scale_color_manual(values=c("Observed"="black", "Random"="red")) +
+      scale_color_manual(values=c("Observed"="red", "Random"="gray50")) +
+      scale_fill_manual(values=c("Observed"="red", "Random"="gray50")) +
       facet_wrap(~ V5, ncol=2, scales='free') +
       theme_nature +
       theme(axis.title.y=element_text(angle=0),
             axis.text=element_text(size=5),
             axis.text.x=element_text(angle=-90, hjust=0, vjust=.5),
             legend.position="right",
-            panel.margin=unit(rep(5, 4), 'mm'),
-            plot.margin=unit(c(1, 0, 0, 0), 'mm')
+            panel.margin=unit(rep(1, 4), 'mm')
             ))
 Cairo(dpi='auto', file=sub('.txt', '.pdf', args[1]), height=190, type='pdf', units='mm', width=160)
 print(p)
