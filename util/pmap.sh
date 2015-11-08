09425a2ae3fde5981468261c1fc9249881493335
Abhishek Sarkar <aksarkar@mit.edu>
2015-11-08 10:45:42 -0500

    Fix deprecated argument

--- util/pmap.sh
+++ util/pmap.sh
@@ -20,4 +20,4 @@ set -u
 # UGER munges LD_LIBRARY_PATH and doesn't pass it through even with -V, so fix
 # it up here
 export LD_LIBRARY_PATH=$LIBRARY_PATH
-awk -vn=$SGE_TASK_LAST -vi=$SGE_TASK_ID 'NR % n == i - 1' $1 | parallel -j1 --halt 1 -t
+awk -vn=$SGE_TASK_LAST -vi=$SGE_TASK_ID 'NR % n == i - 1' $1 | parallel -j1 --halt now,fail=1 -t
