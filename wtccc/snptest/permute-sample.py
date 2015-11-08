4c7eb074085305c1f17b348df4777d5d3f22266a
Abhishek Sarkar <aksarkar@mit.edu>
2015-11-08 10:46:03 -0500

    Properly extract phenotype with covariates

--- wtccc/snptest/permute-sample.py
+++ wtccc/snptest/permute-sample.py
@@ -1,13 +1,14 @@
+import gzip
 import random
 import sys
 
-with open(sys.argv[1]) as f:
+with gzip.open(sys.argv[1], 'rt') as f:
     data = (line.split() for line in f)
     print(*next(data))
     print(*next(data))
     samples = list(data)
     random.seed(int(sys.argv[2]))
-    pheno = [int(x[-1]) - 1 for x in samples]
+    pheno = [int(x[6]) for x in samples]
     random.shuffle(pheno)
     for s, p in zip(samples, pheno):
-        print(' '.join(s[:-1]), p)
+        print(' '.join(s[:6]), p, ' '.join(s[7:]))
