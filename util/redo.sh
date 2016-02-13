b419074ff5c89c47def45c27300fe0352d74cc29
Abhishek Sarkar <aksarkar@mit.edu>
2016-02-13 11:51:07 -0500

    Improve pipeline redo script
    Parse GNU parallel joblog output to get tasks to be redone

--- util/redo.sh
+++ util/redo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Usage: redo.sh JOBLIST
 set -e
-awk 'NR > 1 && ! /code 0/' | perl -ne 'm/(?:\.)([0-9]+)/; print $1 . "p\n"' | sed -nf - $1
+set -u
+awk -vFS='\t' '$1 != "Seq" && ($7 != 0 || $8 != 0) {print $9}' $*
