4cce90190192c44a1fabff3a426871df1e45149f
Abhishek Sarkar <aksarkar@mit.edu>
2017-08-05 10:50:31 -0400

    Properly handle empty input

--- util/redo.sh
+++ util/redo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
 set -u
-awk -vFS='\t' '$1 != "Seq" && ($7 != 0 || $8 != 0) {print $9}' $*
+if [[ -f $* ]]
+then
+    awk -vFS='\t' '$1 != "Seq" && ($7 != 0 || $8 != 0) {print $9}' $*
+fi
